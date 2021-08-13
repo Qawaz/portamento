ac91dad5ef5fde46b6f4b99f24dda5bab2ecceb2
Waqas Tahir <wakaztahir@gmail.com>
2021-08-13 15:09:26 +0500

    without lib interference , Unable to create media player error

--- app/src/main/java/com/wakaztahir/portamentoplayer/MainActivity.kt
+++ app/src/main/java/com/wakaztahir/portamentoplayer/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.wakaztahir.portamentoplayer
 
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import com.wakaztahir.portamento.Portamento
-import com.wakaztahir.portamento.components.defaultPlayer
-import com.wakaztahir.portamento.play
-import com.wakaztahir.portamento.rememberPortamentoState
+import androidx.compose.runtime.remember
 import com.wakaztahir.portamentoplayer.ui.theme.PortamentoPlayerTheme
 import java.io.File
 
@@ -25,18 +27,38 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
 
                     //Portamento States
-                    val state = rememberPortamentoState()
-                    val currentFile by mutableStateOf(File("//android_asset/1.mp3"))
+                    val currentFile by mutableStateOf("1.mp3")
 
-                    Portamento(
-                        state = state,
-                        path = currentFile.absolutePath,
-                        onInitialized = {
-                            play()
+                    val mediaPlayer = remember { MediaPlayer() }
+
+                    LaunchedEffect(key1 = null, block = {
+                        kotlin.runCatching {
+
+                            val x = assets.open(currentFile)
+                            val tempFile = openFileOutput("active.mp3", Context.MODE_PRIVATE)
+                            tempFile.write(x.readBytes())
+                            tempFile.close()
+                            x.close()
+                            val file = File(filesDir.absolutePath + File.separator + "active.mp3")
+
+                            if (file.canRead()) {
+                                Log.d("TL_CreatingCopyFile", "Can Read")
+                            }
+
+                            mediaPlayer.setAudioAttributes(
+                                AudioAttributes.Builder()
+                                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build()
+                            )
+                            mediaPlayer.setDataSource(file.absolutePath)
+                            Log.d("TL_CreatingCopyFile", "Data Source Set")
+                            mediaPlayer.prepareAsync()
+                            mediaPlayer.setOnPreparedListener {
+                                it.start()
+                            }
+                            Log.d("TL_CreatingCopyFile", "Work Ended")
                         }
-                    ) {
-                        defaultPlayer()
-                    }
+                    })
                 }
             }
         }
